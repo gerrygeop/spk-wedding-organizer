9195d71a3a2a4134522d5d6a745a7c5d7183af14
gerrygeop <gerryja28@gmail.com>
2023-05-30 14:48:32 +0800

    new components

---
+++ resources/js/Components/Container.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Container({ children }) {
+    return (
+        <div className="py-12">
+            <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">{children}</div>
+        </div>
+    );
+}
+
+export const Board = ({ children, className }) => {
+    return (
+        <div
+            className={`bg-white overflow-hidden shadow sm:rounded-lg ${className}`}
+        >
+            {children}
+        </div>
+    );
+};
+
+export const Section = ({ children, className }) => {
+    return (
+        <div className={`py-5 px-4 sm:px-6 sm:py-8 ${className}`}>
+            {children}
+        </div>
+    );
+};
---
+++ resources/js/Components/Table.jsx
@@ -0,0 +1,57 @@
+const Table = ({ children }) => {
+    return (
+        <div className="flex flex-col">
+            <div className="-mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                    <div className="overflow-hidden">
+                        <table className="min-w-full">{children}</table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const Thead = ({ children, className }) => {
+    return (
+        <thead className={`bg-white border-b border-gray-300 ${className}`}>
+            {children}
+        </thead>
+    );
+};
+
+const Tbody = ({ children, className }) => {
+    return (
+        <tbody className={`bg-white divide-y divide-gray-200 ${className}`}>
+            {children}
+        </tbody>
+    );
+};
+
+const Th = ({ children, className, ...props }) => {
+    return (
+        <th
+            {...props}
+            className={`px-4 py-3 text-gray-500 text-left text-xs font-semibold uppercase tracking-wider ${className}`}
+        >
+            {children}
+        </th>
+    );
+};
+const Td = ({ children, className, ...props }) => {
+    return (
+        <td
+            {...props}
+            className={`px-4 py-4 whitespace-nowrap font-medium text-gray-600 ${className}`}
+        >
+            {children}
+        </td>
+    );
+};
+
+Table.Thead = Thead;
+Table.Tbody = Tbody;
+Table.Th = Th;
+Table.Td = Td;
+
+export default Table;
