450c89aac77246464fa4c41f18a76ea26995b4c0
gerrygeop <gerryja28@gmail.com>
2023-06-02 20:19:05 +0800

    update max width

--- resources/js/Layouts/AdminLayout.jsx
+++ resources/js/Layouts/AdminLayout.jsx
@@ -6,7 +6,7 @@ export default function AdminLayout({ children }) {
         <div className="min-h-screen bg-gray-100">
             <Navbar />
 
-            <div className="flex max-w-7xl mx-auto">
+            <div className="flex max-w-[100rem] mx-auto">
                 <Sidebar />
 
                 <main className="flex-1 ml-0 lg:ml-64">{children}</main>
--- resources/js/Layouts/Navbar.jsx
+++ resources/js/Layouts/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar() {
 
     return (
         <nav className="bg-white border-b border-gray-100">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="max-w-[100rem] mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16">
                     <div className="flex">
                         <div className="shrink-0 flex items-center">
